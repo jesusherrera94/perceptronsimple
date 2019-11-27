9ba298b28ccabe2e371672d3896a48b300029d86
Jesus Herrera <jesusherrera1594@gmail.com>
2019-11-27 08:09:35 -0600

    PerceptronSimple Taller

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ inputs = '{"table":[{"in":[0,0]},{"in":[0,1]},{"in":[1,0]},{"in":[1,1]}]}'
 
 dbExtracted = connection()
 result = dbExtracted.getDB("-Luh2RGCkrEBcoTABzea")
+print(result["OR"])
 
 perceptron = Perceptron(inputs, None, None)
 perceptron.runPerceptron(result["OR"])
